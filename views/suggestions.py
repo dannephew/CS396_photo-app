a14c8c047fbd7b357b171ab5d54eec9aab91d7cf
dannephew <danneszhang@gmail.com>
2022-02-11 18:44:01 -0600

    complete assignment

--- views/suggestions.py
+++ views/suggestions.py
@@ -10,21 +10,27 @@ class SuggestionsListEndpoint(Resource):
         self.current_user = current_user
     
     def get(self):
-        pass
         # Your code here:
         #get all the users that current user isn't follow 
         
         #ids that cannot be recommended: 
         
-        # auth_users_ids = get_authorized_user_ids(self.current_user)
+        auth_users_ids = get_authorized_user_ids(self.current_user)
         
         # all_users = User.query.all()
+        # recommendations = User.query.filter_by(id=self.current_user.id)
         
-        # for user in all_users:
-        #     for auth_user in auth_users_ids:
-        #         #check if there's a match
-        #         #if there is not a match, then you can add it to a list people you can recommend
-        # return Response(json.dumps([]), mimetype="application/json", status=200)
+        recommendations = User.query.filter(User.id.not_in(auth_users_ids)).limit(7)
+        
+        
+        # recommendations = User.query.filter(User.id.not_in(auth_users_ids)).all()
+
+        
+        dict = [
+            recommendation.to_dict() for recommendation in recommendations
+        ]
+        
+        return Response(json.dumps(dict), mimetype="application/json", status=200)
 
 
 def initialize_routes(api):
