76514a80bd47040a508e51f49284027367a98e29
dannephew <danneszhang@gmail.com>
2022-02-23 04:05:35 -0600

    hw4 post implementation

--- models/post.py
+++ models/post.py
@@ -1,13 +1,10 @@
 from datetime import datetime
-import random
 from . import db
+from .utils import get_display_time
 
 class Post(db.Model):
-    # name of table I want to connect to:
-
     __tablename__ = 'posts'
-    # reference to the columns with which I want the application
-    # to interact:
+
     id = db.Column(db.Integer, primary_key=True)
     image_url = db.Column(db.String(200), nullable=False)
     caption = db.Column(db.Text, nullable=True)
@@ -18,13 +15,11 @@ class Post(db.Model):
         nullable=False)
 
     # read-only property for referencing User properties
-    # convenience methods that allow me to join to other tables:
-
     user = db.relationship('User', backref="posts", lazy=False)
     comments = db.relationship('Comment', cascade="all,delete-orphan", lazy='select',
         backref=db.backref('posts', lazy='joined'))
-
-    # constructor function for creating new "post" records in the DB
+    likes = db.relationship('LikePost', cascade="all,delete-orphan", lazy='select',
+        backref=db.backref('posts', lazy='joined'))
 
     def __init__(self, image_url, user_id, caption=None, alt_text=None, pub_date=None):
         self.image_url = image_url
@@ -36,17 +31,38 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post={0} by User={1}>'.format(self.id, self.user_id)
 
-    def to_dict(self, include_comments=True): 
+
+    def to_dict(self, 
+            include_comments=True, 
+            include_likes=True,
+            user=None): 
         d = {
             'id': self.id,
             'image_url': self.image_url,
             'user': self.user.to_dict(),
             'caption': self.caption, 
-            'alt_text': self.alt_text
+            'alt_text': self.alt_text,
+            'display_time': get_display_time(self.pub_date)
         }
+        if include_likes:
+            d['likes'] = [like.to_dict() for like in self.likes]
+        
         if include_comments:
             d['comments'] = [
                 comment.to_dict() for comment in self.comments
             ]
+
+        # some convenience properties of the post:
+        if user:
+            def filter_function(item):
+                return item.user_id == user.id
+
+            user_like_id_list = [like.id for like in filter(filter_function, self.likes)]
+            user_bookmark_id_list = [bookmark.id for bookmark in filter(filter_function, self.bookmarks)]
+            if len(user_like_id_list) == 1:
+                d['current_user_like_id'] = user_like_id_list[0]
+            if len(user_bookmark_id_list) == 1:
+                d['current_user_bookmark_id'] = user_bookmark_id_list[0]
+        
         return d
  
--- views/posts.py
+++ views/posts.py
@@ -1,14 +1,13 @@
 from flask import Response, request
 from flask_restful import Resource
-from models import Post, User, db
-from . import can_view_post, get_authorized_user_ids
+from models import Post, db
+
+from views import security, get_authorized_user_ids
+
 import json
-from sqlalchemy import and_
-from post_dec import check_int, check_invalid_unauthorized, DEL_check_int, DEL_check_invalid_unauthorized
 
 def get_path():
     return request.host_url + 'api/posts/'
-
 
 class PostListEndpoint(Resource):
 
@@ -16,14 +15,9 @@ class PostListEndpoint(Resource):
         self.current_user = current_user
 
     def get(self):
-        # TODO: 
-        # 1. No security implemented; 
-        # 2. limit is hard coded (versus coming from the query parameter)
-        # 3. No error checking
-        
-        #get all posts of user
+        ids = get_authorized_user_ids(self.current_user)
+        posts = Post.query.filter(Post.user_id.in_(ids))
         limit = request.args.get('limit')
-        print("FIRST LIMIT: ", limit)
         if limit:
             try:
                 limit = int(limit)
@@ -32,110 +26,78 @@ class PostListEndpoint(Resource):
             if limit > 50 or limit < 1:
                 return Response(json.dumps({'message': 'Limit must be an integer between 1 and 50'}), mimetype="application/json", status=400)
         else:
-            limit = 20
-        print("LIMIT: ", limit)
-        posts = Post.query.filter_by(user_id=self.current_user.id).order_by(Post.pub_date.desc()).limit(limit).all()
+            limit = 10
+        posts = posts.order_by(Post.pub_date.desc()).limit(limit)
 
-        # posts = posts.limit(3)
-        print("POSTS: ", posts)
-        #print(bookmarks)
-        
-        # convert list of Bookmark model to a list of dictionaries
-        posts_list_of_dictionaries = [
-            post.to_dict() for post in posts
+        # note: if you pass the current user into to_dict(), it will tell you
+        # whether or not the current user liked and/or bookmarked any of the posts
+        data = [
+            item.to_dict(user=self.current_user) for item in posts.all()
         ]
-        
-        # data = Post.query.limit(20).all()
-
-        # data = [
-        #     item.to_dict() for item in data
-        # ]
-        return Response(json.dumps(posts_list_of_dictionaries), mimetype="application/json", status=200)
-
+        return Response(json.dumps(data), mimetype="application/json", status=200)
 
     def post(self):
         body = request.get_json()
         image_url = body.get('image_url')
+        if image_url is None:
+            return Response(json.dumps({'message': '"image_url" is required.'}), mimetype="application/json", status=400)
         caption = body.get('caption')
         alt_text = body.get('alt_text')
-        print(image_url)
-        print(caption, alt_text)
         user_id = self.current_user.id # id of the user who is logged in
-        if not image_url:
-            return Response(json.dumps({'message': 'No image'}), mimetype="application/json", status=400)
+        
         # create post:
         post = Post(image_url, user_id, caption, alt_text)
         db.session.add(post)
         db.session.commit()
-        return Response(json.dumps(post.to_dict()), mimetype="application/json", status=201)
+        return Response(json.dumps(post.to_dict(user=self.current_user)), mimetype="application/json", status=201)
         
 class PostDetailEndpoint(Resource):
 
     def __init__(self, current_user):
         self.current_user = current_user
         
-    @check_int
-    @check_invalid_unauthorized
+    
+    @security.id_is_valid
+    @security.user_can_edit_post
     def patch(self, id):
         post = Post.query.get(id)
-
-        # a user can only edit their own post:
-        if not post or post.user_id != self.current_user.id:
-            return Response(json.dumps({'message': 'Post does not exist'}), mimetype="application/json", status=404)
-       
-
         body = request.get_json()
-        post.image_url = body.get('image_url') or post.image_url
-        post.caption = body.get('caption') or post.caption
-        post.alt_text = body.get('alt_text') or post.alt_text
+
+        post.image_url = body.get('image_url') or body.get('image_url')
+        if body.get('caption') is not None:
+            post.caption = body.get('caption')
+        if body.get('alt_text') is not None:
+            post.alt_text = body.get('alt_text')
         
         # commit changes:
         db.session.commit()        
-        return Response(json.dumps(post.to_dict()), mimetype="application/json", status=200)
+        return Response(json.dumps(post.to_dict(user=self.current_user)), mimetype="application/json", status=200)
     
-    @DEL_check_int
-    @DEL_check_invalid_unauthorized
+    @security.id_is_valid
+    @security.user_can_edit_post
     def delete(self, id):
-
-        # a user can only delete their own post:
-        post = Post.query.get(id)
-        if not post or post.user_id != self.current_user.id:
-            return Response(json.dumps({'message': 'Post does not exist'}), mimetype="application/json", status=404)
-       
-
         Post.query.filter_by(id=id).delete()
         db.session.commit()
+
         serialized_data = {
             'message': 'Post {0} successfully deleted.'.format(id)
         }
         return Response(json.dumps(serialized_data), mimetype="application/json", status=200)
 
-    @DEL_check_int
-    @DEL_check_invalid_unauthorized
+    @security.id_is_valid
+    @security.user_can_view_post
     def get(self, id):
         post = Post.query.get(id)
-
-        # if the user is not allowed to see the post or if the post does not exist, return 404:
-        if not post or not can_view_post(post.id, self.current_user):
-            return Response(json.dumps({'message': 'Post does not exist'}), mimetype="application/json", status=404)
-        
-        return Response(json.dumps(post.to_dict()), mimetype="application/json", status=200)
+        return Response(json.dumps(post.to_dict(user=self.current_user)), mimetype="application/json", status=200)
 
 def initialize_routes(api):
-    #PostListEndpoint: GET, POST
     api.add_resource(
         PostListEndpoint, 
         '/api/posts', '/api/posts/', 
         resource_class_kwargs={'current_user': api.app.current_user}
     )
-
-    #Detail: Post (single), PATCH, DELETE
     api.add_resource(
         PostDetailEndpoint, 
         '/api/posts/<id>', '/api/posts/<id>/',
         resource_class_kwargs={'current_user': api.app.current_user}
     )
-
-
-    # I set up the system with Professor Sarah Van Wart during office hours, which took a while. Postgres was not being recognized as a valid role, 
-    # so I had to run the following command: ALTER DATABASE postgres OWNER TO postgres;
